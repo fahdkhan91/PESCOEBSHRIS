8db0943349851174b1fc777e7837188de744459e
fahdkhan91 <fahdkhan91@users.noreply.github.com>
2026-04-10 00:28:32 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2640,6 +2640,4 @@ def bulk_salary_slips():
 
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(debug=True)
